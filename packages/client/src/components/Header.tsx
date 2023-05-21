cc134334d061f3da6bf56a5307f359476982517c
Piotr Filipiak <kiddhkane@gmail.com>
2023-05-21 22:44:11 +0200

    Styling header a bit.

--- packages/client/src/components/Header.tsx
+++ packages/client/src/components/Header.tsx
@@ -1,5 +1,24 @@
 const Header = () => {
-  return <div>Header</div>;
+  return (
+    <header className="bg-slate-300 flex justify-center">
+      <div className="container bg-slate-400 flex justify-between">
+        <a href="logo">Logo</a>
+        <nav>
+          <menu className="flex gap-2">
+            <li>
+              <a href="navLink">Link</a>
+            </li>
+            <li>
+              <a href="navLink">Link</a>
+            </li>
+            <li>
+              <a href="navLink">Link</a>
+            </li>
+          </menu>
+        </nav>
+      </div>
+    </header>
+  );
 };
 
 export default Header;
