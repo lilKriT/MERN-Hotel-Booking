536fe25dc901e2d870d1c36639cafc71b58e5495
Piotr Filipiak <kiddhkane@gmail.com>
2023-05-21 23:30:05 +0200

    Started adding date range picker. Got it to work somehow.

--- packages/client/src/components/Search.tsx
+++ packages/client/src/components/Search.tsx
@@ -1,7 +1,36 @@
+import { useState } from "react";
 import { DateRange } from "react-date-range";
 
+import "react-date-range/dist/styles.css"; // main css file
+import "react-date-range/dist/theme/default.css"; // theme css file
+
+interface IDateRange {
+  startDate?: Date;
+  endDate?: Date;
+  key?: string;
+}
+
 const Search = () => {
-  return <div>Search</div>;
+  const [state, setState] = useState<IDateRange[]>([
+    {
+      startDate: new Date(),
+      endDate: undefined,
+      key: "selection",
+    },
+  ]);
+
+  return (
+    <form className="border border-sky-600">
+      <input type="text" />
+      <input type="text" />
+      <DateRange
+        editableDateInputs={true}
+        onChange={(item) => setState([item.selection])}
+        moveRangeOnFirstSelection={false}
+        ranges={state}
+      />
+    </form>
+  );
 };
 
 export default Search;
