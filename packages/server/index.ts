bc495ba02daf45f6ed3602be0756bbc534bc840c
Piotr Filipiak <kiddhkane@gmail.com>
2023-05-26 00:41:17 +0200

    Connecting to Mongo. Created a hotel model.

--- packages/server/index.ts
+++ packages/server/index.ts
@@ -2,12 +2,15 @@ import express from "express";
 import dotenv from "dotenv";
 import colors from "colors";
 
+import connectDB from "./config/db";
+
 // Basic config
 const app = express();
 dotenv.config({ path: "../../.env" });
 const PORT = process.env.PORT || 3000;
 
-// Listening
+// Connecting and listening
+connectDB();
 app.listen(PORT, () =>
   console.log(colors.green.underline(`Express listening on port: ${PORT}`))
 );
