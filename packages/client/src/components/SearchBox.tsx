f3a69a6840c502c36531414059dd0ab729d32fca
Piotr Filipiak <kiddhkane@gmail.com>
2023-05-25 21:10:30 +0200

    Search box done.

--- packages/client/src/components/SearchBox.tsx
+++ packages/client/src/components/SearchBox.tsx
@@ -3,6 +3,7 @@ import IDateRange from "../interfaces/IDateRange";
 import IRoomOptions from "../interfaces/IRoomOptions";
 import { format } from "date-fns";
 import RoomOptions from "./RoomOptions";
+import { DateRange } from "react-date-range";
 
 interface ISearchBox {
   state: {
@@ -16,7 +17,10 @@ const SearchBox = ({ state }: ISearchBox) => {
   const [destination, setDestination] = useState(state.destination);
   const [dates, setDates] = useState(state.dates);
   const [roomOptions, setRoomOptions] = useState(state.roomOptions);
+
+  // Settings
   const [roomOptionsOpened, setRoomOptionsOpened] = useState(false);
+  const [datePickerOpened, setDatePickerOpened] = useState(false);
 
   // Handling option changes
   type RoomOptionsKey = keyof typeof roomOptions;
@@ -31,6 +35,11 @@ const SearchBox = ({ state }: ISearchBox) => {
     setRoomOptions({ ...roomOptions, [castOption]: newValue });
   };
 
+  // Handling new search
+  const handleReSearch = () => {
+    console.log("Searching");
+  };
+
   return (
     // Form begins
     <form>
@@ -42,14 +51,32 @@ const SearchBox = ({ state }: ISearchBox) => {
       />
 
       {/* Dates Field */}
-      <span>{`${format(
-        dates[0].startDate || Date.now(),
-        "MM/dd/yy"
-      )} to ${format(dates[0].endDate || Date.now(), "MM/dd/yy")}`}</span>
+      <div className="relative">
+        <span
+          className="cursor-pointer"
+          onClick={() => setDatePickerOpened(!datePickerOpened)}
+        >{`${format(dates[0].startDate || Date.now(), "MM/dd/yy")} to ${format(
+          dates[0].endDate || Date.now(),
+          "MM/dd/yy"
+        )}`}</span>
+
+        {/* Date Picker */}
+        {datePickerOpened && (
+          <DateRange
+            editableDateInputs={true}
+            onChange={(item) => setDates([item.selection])}
+            moveRangeOnFirstSelection={false}
+            ranges={dates}
+            minDate={new Date()}
+            className="absolute top-10 left-0"
+          />
+        )}
+      </div>
 
       {/* Room Options Field */}
       <div className="bg-slate-200 relative">
         <span
+          className="cursor-pointer"
           onClick={() => setRoomOptionsOpened(!roomOptionsOpened)}
         >{`${roomOptions.adults} adults - ${roomOptions.children} children - ${roomOptions.rooms} rooms`}</span>
 
@@ -61,6 +88,14 @@ const SearchBox = ({ state }: ISearchBox) => {
           />
         )}
       </div>
+
+      <button
+        className="btn btn--primary"
+        type="button"
+        onClick={() => handleReSearch()}
+      >
+        Search again!
+      </button>
     </form>
   );
 };
