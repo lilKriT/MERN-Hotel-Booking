f71579453254b944371cc61af64a27e89b738d76
Piotr Filipiak <kiddhkane@gmail.com>
2023-05-25 22:04:21 +0200

    Added simple hotel cards.

--- packages/client/src/components/HotelCard.tsx
+++ packages/client/src/components/HotelCard.tsx
@@ -3,7 +3,13 @@ interface IHotelCard {
 }
 
 const HotelCard = ({ hotel }: IHotelCard) => {
-  return <div className="h-40">HotelCard</div>;
+  return (
+    <div className="bg-slate-300 mb-8 p-8 rounded-lg">
+      <h2>{hotel.name}</h2>
+      <p>{hotel.rating}</p>
+      <p>{hotel.price}</p>
+    </div>
+  );
 };
 
 export default HotelCard;
